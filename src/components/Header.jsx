b8b673e74f6c7c1d9a2bed51ca0475a12e5a049b
Josef1923 <schererjos01@gmail.com>
2025-01-10 22:34:09 +0100

    Build header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,33 @@
   Composant Header à utiliser sur les différentes pages du site.
 */
 
-import { Link } from "react-router-dom" 
+import { NavLink } from "react-router-dom"
 import "../Styles/Header.css"
 
 function Header() {
   return (
     <header className="header">
-      <nav>
-        <Link to="/" >Accueil</Link>
-        <Link to="/a-propos" >À propos</Link>
-        <Link to="/logement/1">Fiche logement 1</Link>
-        <Link to="/*" >Erreur 404</Link>
+      <div className="logo">
+        <picture>
+          <source srcSet="src/assets/Logo/LogoSmall.png" media="(max-width: 768px)" />
+          <source srcSet="src/assets/Logo/LogoBig.png" media="(min-width: 769px)" />
+          <img src="src/assets/Logo/LogoBig.png" alt="Kasa Logo" />
+        </picture>
+      </div>
+      <nav className="headerLinks">
+        <ul>
+          <li>
+            <NavLink to="/"
+            className={({ isActive}) => (isActive ? "activeLink" : "" )}>Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink to="/a-propos"
+              className={({ isActive }) => (isActive ? "activeLink" : "")}>A propos</NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
 }
- export default Header
+
+export default Header
