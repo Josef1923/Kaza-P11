e8ea097900fb3bd78fca2a453f9358512e527a8e
Josef1923 <schererjos01@gmail.com>
2025-01-11 18:19:15 +0100

    Build Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,27 @@
   Chaque route correspond à une page spécifique.
 */
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import APropos from './pages/APropos';
-import FicheLogement from './pages/FicheLogement';
-import Error from './pages/404';
-import Header from './components/Header';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import APropos from "./pages/APropos";
+import FicheLogement from "./pages/FicheLogement";
+import Error from "./pages/404";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/a-propos" element={<APropos />} />
-        <Route path="/logement/:id" element={<FicheLogement />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
-  )
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/a-propos" element={<APropos />} />
+          <Route path="/logement/:id" element={<FicheLogement />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,20 @@
   Composant Footer à utiliser sur les différentes pages du site.
 */
 
-export function Footer ()  {
-    return 'Footer'
+import "../Styles/Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="logo">
+        <img src="src/assets/Logo/LogoFooter.png" alt="Kasa Logo" />
+      </div>
+      <p>
+        <span>© 2020 Kasa. All</span>
+        <span>rights reserved</span>
+      </p>
+    </footer>
+  );
 }
+
+export default Footer
