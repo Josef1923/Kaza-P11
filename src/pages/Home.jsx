7a3f23b67466d8ac39da5ede8e7ac77a9b85e03d
Josef1923 <schererjos01@gmail.com>
2025-01-12 12:44:40 +0100

    Push pour modifier Banner et Temp Banner (et remodifier prochain push) Probeleme de A majuscule

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@
 import Banner from "../components/TempBanner"
 import BannerHome from "../assets/Images/BannerHome.png"
 
-function Home () {
-    return (
-        <div>
-            <Banner images= {BannerHome} />
-            </div>
-    )
+function Home() {
+  return (
+    <div>
+      <Banner images={BannerHome} />
+    </div>
+  )
 }
 
 export default Home
