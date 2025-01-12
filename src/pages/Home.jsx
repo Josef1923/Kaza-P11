27570dcf78b81c48de564230fac89164a804af9a
Josef1923 <schererjos01@gmail.com>
2025-01-12 22:04:08 +0100

    Ajout container pour page home en vu d'y mettre les cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,16 @@
 
 import Banner from "../components/TempBanner"
 import BannerHome from "../assets/Images/BannerHome.png"
+import "../Styles/Home.css"
 
 function Home() {
   return (
     <div>
+    <div>
       <Banner images={BannerHome} className="HomeBannerImg" 
       text = "Chez vous, partout et ailleurs"/>
+    </div>    
+    <div className="HomeContainer"></div>
     </div>
   )
 }
