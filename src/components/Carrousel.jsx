b0fe4e7a8e7fc086f05ffa69d6f1d38d65b687fc
Josef1923 <schererjos01@gmail.com>
2025-01-20 12:01:25 +0100

    Ajout fonction de slide gauche droite

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -2,17 +2,28 @@ import PropTypes from "prop-types"
 import "../styles/Carrousel.css"
 import CarrouselLeftSlideVector from "../assets/svg/CarrouselLeftSlideVector.svg"
 import CarrouselRightSlideVector from "../assets/svg/CarrouselRightSlideVector.svg"
+import { useState } from "react"
 
 function Carrousel({ picture }) {
 
+    const [currentImageIndex, setIndex] = useState(0)
+
+    const rightSlide = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % picture.length)
+    }
+    
+    const leftSlide = () => {
+        setIndex((prevIndex) => prevIndex === 0 ? picture.length -1 : prevIndex -1)
+    }
+
     return (
         <div className="carrouselContainer">
-            <img src={picture[0]} className="carrouselPicture" ></img>
+            <img src={picture[currentImageIndex]} className="carrouselPicture" ></img>
             <span className="leftSlide">
-                <img src={CarrouselLeftSlideVector} />
+                <img src={CarrouselLeftSlideVector} onClick={leftSlide}/>
             </span>
             <span className="rightSlide">
-                <img src={CarrouselRightSlideVector} />
+                <img src={CarrouselRightSlideVector} onClick={rightSlide}/>
             </span>
         </div>
     )
