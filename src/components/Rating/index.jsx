d4df34ca1c75019a9b9dfef434a7e8c98447e7b4
Josef1923 <schererjos01@gmail.com>
2025-01-22 16:51:31 +0100

    Ajout crochet sur rate

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -6,16 +6,14 @@ function Rating({ rate }) {
 
     let stars = [];
 
-    for (let i = 1; i <= 5; i++)
+    for (let i = 1; i <= 5; i++) {
         if (i <= rate) {
-            stars.push(<img key={i} src={ColoredStar} className="coloredStar"  alt={`Étoile ${i} sur ${rate} - remplie`}></img>)
+            stars.push(<img key={i} src={ColoredStar} className="coloredStar" alt={`Étoile ${i} sur ${rate} remplie`}></img>)
+        } else {
+            stars.push(<img key={i} src={EmptyStar} className="emptyStar" alt={`Étoile ${i} sur ${rate} vide`}></img>)
         }
-
-        else {
-            stars.push(<img key={i} src={EmptyStar} className="emptyStar"  alt={`Étoile ${i} sur ${rate} - vide`}></img>)
-        }
-
-        return (
+    }
+    return (
         <div className="rating">{stars}</div>
     )
 }
